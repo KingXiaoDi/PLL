403abb36bfcbcfde14ea51a632cfa79071aa817c
unknown <organizedchaos20@gmail.com>
2021-02-13 22:15:23 -0500

    Update Main Script With New Functions
    Made numerous data changes:
    -added a true game ID (official PLL game ID)
    -added games.csv, which has a row for each game in 2020 with the following columns:
        -season
        -old gameID is now gameNum (game number)
        -gameID now matches PLL official gameID
        -game date and start time
        -each team, their odds, and their final score
        -over/under
        -game type (group/knockout/reg/post)
        -whether the game went to OT
        -length of the game (approximate, actually length of video)
    -updated column names in goals.csv to remove spaces and make them lowercase
    
    Updated goal loader function to work with new data format.
    
    Added functions to:
    -determine which team is leading/trailing and their current (post-goal) score
    -get the final score for each game (then added to games.csv)
    -determine the pre-goal score and add it to the goals table (to determine score_diff)
    -update the names for the scorer/assister/goalie to their full name and include their UUID

--- messingAround.R
+++ messingAround.R
@@ -5,11 +5,11 @@ library(ggimage)
 load_goals <- function() {
   return (read_csv('data/PLL Goals.csv') %>% 
             filter(!is.na(time)) %>%
-            group_by(game) %>% 
+            group_by(gameID) %>% 
             mutate(time_q = as.POSIXct(time, format = '%M:%S'),
                    time_r = as.POSIXct(time_q + (4-q)*12*60, format ='%M:%S.%f'),
                    elapsed = 48-(minute(time_r) + second(time_r)/60)) %>% 
-            arrange(game, q, desc(time_r)) %>% 
+            arrange(gameID, q, desc(time_r)) %>% 
             mutate(between = difftime(lag(time_r), time_r, units='secs'),
                    test = case_when(is.na(between)~ difftime(as.POSIXct('48:00.00', format='%M:%S'), time_r, units='secs'),
                                     TRUE ~ 0),
@@ -17,14 +17,85 @@ load_goals <- function() {
                                        !is.na(test)~test)) %>% 
             group_by(date, off) %>% 
             mutate(points = twoPT + 1,
-                   score = cumsum(points)) %>% 
+                   score = cumsum(points),
+                   numA = case_when(numA == 'N/A'~NA_real_,
+                                    TRUE~as.numeric(numA)),
+                   assist = case_when(assist == 'N/A'~NA_character_,
+                                      TRUE~assist)) %>% 
             ungroup())
+}
+
+add_score_lead_trail <- function(game) {
+  
+  score <- game %>% 
+    pivot_wider(id_cols = c('gameID','goalID'), names_from='off', values_from='score') %>% 
+    fill(3:4)
+  
+  teamA <- colnames(score)[3]
+  teamB <- colnames(score)[4]
+  
+  score %>% 
+    mutate(!!as.symbol(teamA) := case_when(is.na(!!as.symbol(teamA))~0,
+                                           TRUE~!!as.symbol(teamA)),
+           !!as.symbol(teamB) := case_when(is.na(!!as.symbol(teamB))~0,
+                                           TRUE~!!as.symbol(teamB)),
+           leader = case_when(!!as.symbol(teamA)>=!!as.symbol(teamB)~teamA,
+                              !!as.symbol(teamA)<!!as.symbol(teamB)~teamB,
+                              TRUE ~ NA_character_),
+           leadScore = case_when(!!as.symbol(teamA)>=!!as.symbol(teamB)~!!as.symbol(teamA),
+                                 !!as.symbol(teamA)<!!as.symbol(teamB)~!!as.symbol(teamB),
+                                 TRUE ~ NA_real_),
+           trailer = case_when(!!as.symbol(teamA)>=!!as.symbol(teamB)~teamB,
+                               !!as.symbol(teamA)<!!as.symbol(teamB)~teamA,
+                               TRUE ~ NA_character_),
+           trailScore = case_when(!!as.symbol(teamA)>=!!as.symbol(teamB)~!!as.symbol(teamB),
+                                  !!as.symbol(teamA)<!!as.symbol(teamB)~!!as.symbol(teamA),
+                                  TRUE ~ NA_real_))
+}
+
+get_final_scores <- function(games, goals) {
+  
+  gameIDs <- games %>% pull(gameID)
+  
+  results <- tibble()
+  
+  for (game in gameIDs) {
+    
+    gameGoals <- goals %>% filter(gameID == game)
+    gameScore <- add_score_lead_trail(gameGoals)
+    finalScore <- gameScore %>% filter(goalID == max(goalID)) %>% 
+      select(c(gameID, 5:8))
+    results <- bind_rows(results, finalScore)
+  }
+  
+  return (results)
+}
+
+add_score_to_goals <- function(games, goals) {
+  
+  gameIDs <- games %>% pull(gameID)
+  
+  goals_with_scores <- tibble()
+  
+  for (game in gameIDs) {
+    
+    gameGoals <- goals %>% filter(gameID == game)
+    gameScore <- add_score_lead_trail(gameGoals)
+    
+    game_with_scores <- inner_join(gameGoals, gameScore, by=c('gameID', 'goalID')) %>% 
+      mutate(off_score = if_else(off == leader, leadScore, trailScore)-(points),
+             def_score = if_else(def == leader, leadScore, trailScore)) %>% 
+      select(-c(37:42))
+    goals_with_scores <- bind_rows(goals_with_scores, game_with_scores)
+  }
+  
+  return (goals_with_scores)
 }
 
 get_scores_for_team <- function(goals, team) {
   teamGoals <- goals %>% 
     filter(off == team|def == team) %>%
-    group_by(game) %>% 
+    group_by(gameID) %>% 
     mutate(team = team,
            goalTeamA = case_when(off == team ~ 1,
                                  TRUE ~ 0),
@@ -58,7 +129,7 @@ get_time_played_by_score <- function(data) {
 
 determine_Adjustments <- function(data) {
   data %>% 
-    group_by(team, game) %>% 
+    group_by(team, gameID) %>% 
     summarize(OT = case_when(last(q) == 5 ~ 1,
                              TRUE ~ 0),
               finalTime = last(time_r),
@@ -68,9 +139,9 @@ determine_Adjustments <- function(data) {
 
 prepare_Game_Goal_Data_For_Plot <- function(game) {
   return (game %>% 
-            select(game, elapsed, score, goalTeamA, goalTeamB, scoreTeamA, scoreTeamB) %>%
-            add_row(game = 8, elapsed = 0, score = 0, goalTeamA = 1, goalTeamB = 1, scoreTeamA = 0, scoreTeamB = 0) %>% 
-            add_row(game = 8, elapsed = 48, score = 0, goalTeamA = 1, goalTeamB = 1, scoreTeamA = 0, scoreTeamB = 0) %>% 
+            select(gameID, elapsed, score, goalTeamA, goalTeamB, scoreTeamA, scoreTeamB) %>%
+            add_row(gameID = 8, elapsed = 0, score = 0, goalTeamA = 1, goalTeamB = 1, scoreTeamA = 0, scoreTeamB = 0) %>% 
+            add_row(gameID = 8, elapsed = 48, score = 0, goalTeamA = 1, goalTeamB = 1, scoreTeamA = 0, scoreTeamB = 0) %>% 
             mutate(scoreTeamA = case_when(elapsed == 48 ~max(scoreTeamA),
                                           TRUE ~ scoreTeamA),
                    scoreTeamB = case_when(elapsed == 48 ~max(scoreTeamB),
@@ -111,44 +182,82 @@ add_Scoring_Summary_String <- function(row) {
   return (sprintf("(%s, %s) %s scored on %s. Assisted by %s. %s-point goal", 
                   q, time, scorer, goalie, assist, 1+twoPT))
 }
+
+add_unique_IDs_to_goals <- function(goals) {
   
-goals <- load_goals()  
-score <- goals %>% 
-  select(game, date, off, def, q, time_q, points, score, time_r, between, elapsed)
+  roster <- readr::read_csv('data/rosters/combined/2020 combined roster with UUIDs.csv') %>% 
+    separate(name, 
+             c('first', 'last'), 
+             remove=F, 
+             extra = 'merge',
+             fill = 'right')
+  
+  goals_with_ID <- inner_join(goals,
+                              roster,
+                              by=c('off'='team',
+                                   'scorer'='last',
+                                   'numS'='number')) %>%
+    mutate(scorer = name) %>% 
+    rename(scorerID = ID,
+           scorerPos = position) %>% 
+    select(c(season, gameID, q, time, off, def, shot_clock, 
+             scorerID, scorer, scorerPos, 
+             numA, assist, numG, goalie, twoPT, hand, advantage, fast_break, 
+             release, bounce, crease, jump, rebound, points))
+  
+  goals_plus_assists <- left_join(goals_with_ID,
+                                  roster,
+                                  by = c('off'='team',
+                                         'assist'='last',
+                                         'numA'='number')) %>% 
+    mutate(assist = name) %>% 
+    rename(season = season.x,
+           assistID = ID,
+           assistPos = position) %>% 
+    select(c(season, gameID, q, time, def, shot_clock, 
+             scorerID, scorer, scorerPos, assistID, assist, assistPos,
+             numG, goalie, twoPT, hand, advantage, fast_break, 
+             release, bounce, crease, jump, rebound, points))
+  
+  all_players_ID <- inner_join(goals_plus_assists,
+                               roster,
+                               by = c('def'='team',
+                                      'goalie'='last',
+                                      'numG'='number')) %>% 
+    mutate(goalie = name) %>% 
+    rename(season = season.x,
+           goalieID = ID) %>% 
+    select(c(season, gameID, q, time, shot_clock, 
+             scorerID, scorer, scorerPos, assistID, assist, assistPos,
+             goalieID, goalie, twoPT, hand, advantage, fast_break, 
+             release, bounce, crease, jump, rebound, points))
+  
+  return (all_players_ID)
+}
 
-whips <- get_scores_for_team(score, 'Whipsnakes')
-redwoods <- get_scores_for_team(score, 'Redwoods')
-chaos <- get_scores_for_team(score, 'Chaos')
-chrome <- get_scores_for_team(score, 'Chrome')
-archers <- get_scores_for_team(score, 'Archers')
-atlas <- get_scores_for_team(score, 'Atlas')
-dogs <- get_scores_for_team(score, 'Waterdogs')
+goals <- load_goals()
+games <- readr::read_csv('data/games.csv')
 
-teamList <- list(archers, atlas, chaos, chrome, redwoods, dogs, whips)
-lapply(teamList, get_total_time_played)
-lapply(teamList, get_time_played_by_score)
-lapply(teamList, determine_Adjustments)
+add_score_to_goals(games, goals) %>% 
+  readr::write_rds('data/goal data for app.rds')
+
+View(goals)
 
 
-
-goals %>% 
-  mutate(text = sprintf("(%s, %s) %s (%s) scored on %s (%s). Assisted by %s. %s-point goal", 
-                        q, time, scorer, off, goalie, def, assist, 1+twoPT)) %>% pull(text)
-
-
-goals %>%
-  group_by(game, off) %>%
-  summarize(goals = n()) %>% 
-  arrange(game, -goals)
-
-goals %>%
-  group_by(game, off) %>%
-  summarize(goals = n()) %>% 
-  arrange(game, -goals) %>%
-  group_by(game) %>% 
-  summarize(goals = max(goals))
-
-
-goals %>% 
-  filter(twoPT == 1) %>%
-  select(-c(advantage, fast_break))
+old <- {
+  score <- goals %>% 
+    select(gameID, date, off, def, q, time_q, points, score, time_r, between, elapsed)
+  
+  whips <- get_scores_for_team(score, 'Whipsnakes')
+  redwoods <- get_scores_for_team(score, 'Redwoods')
+  chaos <- get_scores_for_team(score, 'Chaos')
+  chrome <- get_scores_for_team(score, 'Chrome')
+  archers <- get_scores_for_team(score, 'Archers')
+  atlas <- get_scores_for_team(score, 'Atlas')
+  dogs <- get_scores_for_team(score, 'Waterdogs')
+  
+  teamList <- list(archers, atlas, chaos, chrome, redwoods, dogs, whips)
+  lapply(teamList, get_total_time_played)
+  lapply(teamList, get_time_played_by_score)
+  lapply(teamList, determine_Adjustments)
+}
